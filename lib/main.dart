1646fd02fee0b8e46ebb07436f3bd84149dea656
zanjabilbenkhedher <zanjabilbenkhedher@gmail.com>
2021-10-25 18:22:54 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,10 @@ void main(){
   runApp (MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
+      backgroundColor: Colors.indigoAccent.shade100,
         appBar:AppBar(
           title: Center(child:Text("I am rich")),
-          backgroundColor: Colors.blueGrey[900],
+          backgroundColor: Colors.indigo.shade900,
         ),
         body:Center(
             child:Image(
